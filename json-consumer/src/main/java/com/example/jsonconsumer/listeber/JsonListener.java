e93dca5c9c524f147cce092dd248756018c7565f
jose <jose.maria.mangueira@gmail.com>
2023-08-03 08:28:46 -0300

    m

--- json-consumer/src/main/java/com/example/jsonconsumer/listeber/JsonListener.java
+++ json-consumer/src/main/java/com/example/jsonconsumer/listeber/JsonListener.java
@@ -1,7 +1,6 @@
 package com.example.jsonconsumer.listeber;
 
 import com.example.jsonconsumer.model.Payment;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
